b34ea6d306ae79d779774f8b8469e7a4883658d1
Axel Fontaine <axel@boxfuse.com>
2022-06-23 13:23:16 +0200

    Added modular kernel detection

--- start.sh
+++ start.sh
@@ -2,30 +2,20 @@
 
 eth0_ip=$(ifconfig eth0 | grep 'inet ' | cut -d ':' -f 2 | cut -d ' ' -f 1)
 eth0_hostname=$(hostname)
+if [ -f /proc/modules ]; then
+  modules="true"
+else
+  modules="false"
+fi
 
 cat > /home/static/healthcheck.json<< EOF
 {
   "healthcheck":true,
   "ip":"$eth0_ip",
-  "hostname":"$eth0_hostname"
+  "hostname":"$eth0_hostname",
+  "modules":$modules
 }
 EOF
 
-log_file=/home/static/thttpd.log
-pid_file=/home/static/thttpd.pid
-
-# Ensure log file gets created first, so there is no race between thttpd and grep
-touch $log_file
-thttpd -D -h 0.0.0.0 -p 80 -d /home/static -u static -l $log_file -i $pid_file -M 60 &
-
-echo "vmify-image-healthcheck is up and running!"
-
-while [ "$(grep 'GET /healthcheck.json HTTP/1.1' $log_file)" = "" ]
-do
-  sleep 1
-done
-
-echo "Access log:"
-cat $log_file
-
-kill "$(cat $pid_file)"
+echo "vmify-image-healthcheck starting ..."
+thttpd -D -h 0.0.0.0 -p 80 -d /home/static -u static -l - -M 60
