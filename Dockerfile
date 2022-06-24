27a3a649c6d564859c8902d1ccdfdc66d86b6017
Axel Fontaine <axel@boxfuse.com>
2022-06-24 13:08:51 +0200

    Added HEALTHCHECK

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,6 @@ WORKDIR /home/static
 ADD start.sh /start.sh
 
 EXPOSE 80
+HEALTHCHECK --start-period=5s --interval=1m --timeout=1s CMD wget -O - http://localhost/healthcheck.json || exit 1
+
 ENTRYPOINT /start.sh
--- test.cmd
+++ test.cmd
@@ -2,6 +2,6 @@ docker build -t healthcheck-test-image .
 docker image inspect healthcheck-test-image
 docker run -d -p 80:80 --name healthcheck-test-container healthcheck-test-image
 curl localhost/healthcheck.json
-docker kill healthcheck-test-container
-docker container rm healthcheck-test-container
-docker image rm healthcheck-test-image
+rem docker kill healthcheck-test-container
+rem docker container rm healthcheck-test-container
+rem docker image rm healthcheck-test-image
