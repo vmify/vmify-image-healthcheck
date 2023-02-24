21a48b2ae3e887400f129de85733b1532bb1cc75
Axel Fontaine <axel@boxfuse.com>
2023-02-24 22:19:18 +0100

    Added mounts and ps

--- start.sh
+++ start.sh
@@ -9,8 +9,10 @@ eth0_hostname=$(hostname)
 eth0_ntp_servers=$(cat /proc/net/ipconfig/ntp_servers)
 
 disks=$(tail +3 /proc/partitions | awk '{ print "\"" $4 "\":" $3*1024 }' | tr '\n' ',' | sed 's/.$//')
+mounts=$(awk '{ print "\"" $2 "\":\"" $1 " " $3 " " $4 " " $5 " " $6 "\"" }' /proc/mounts | sort | tr '\n' ',' | sed 's/.$//')
 swap_total=$(free | tail -1 | tr -s ' ' | cut -d ' ' -f 2)
-envs=$(env | sort | sed 's/=/":"/' | awk '{print "\""$1"\""}'  | tr '\n' ',' | sed 's/.$//')
+envs=$(env | sort | sed 's/=/":"/' | awk '{print "\""$1"\""}' | tr '\n' ',' | sed 's/.$//')
+pss=$(ps ax | tail +2 | head -n -4 | awk '{$2=$3=$4=""; print $0}' | tr -s " " | sed 's/ /":"/' | awk '{print "\"" $0 "\""}' | tr '\n' ',' | sed 's/.$//')
 
 mkdir http
 cd http
@@ -26,8 +28,10 @@ cat > healthcheck.json<< EOF
     "ntp_servers":"$eth0_ntp_servers"
   },
   "disks":{$disks},
+  "mounts":{$mounts},
   "swap_total":$swap_total,
-  "env":{$envs}
+  "env":{$envs},
+  "ps":{$pss}
 }
 EOF
 
