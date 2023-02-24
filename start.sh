e0d6360be3c5727ca1e7d73fa0549ee479cf8e65
Axel Fontaine <axel@boxfuse.com>
2023-02-24 21:06:58 +0100

    Added envs and disks

--- start.sh
+++ start.sh
@@ -2,24 +2,25 @@
 
 eth0_ip=$(ifconfig eth0 | grep 'inet ' | cut -d ':' -f 2 | cut -d ' ' -f 1)
 eth0_hostname=$(hostname)
-if [ -f /proc/modules ]; then
-  modules="true"
-else
-  modules="false"
-fi
+if [ -f /proc/modules ]; then modules="true"; else modules="false"; fi
 swap_total=$(free | tail -1 | tr -s ' ' | cut -d ' ' -f 2)
+disks=$(tail +3 /proc/partitions | awk '{ print "\"" $4 "\":" $3*1024 }' | tr '\n' ',' | sed 's/.$//')
+envs=$(env | sort | sed 's/=/":"/' | awk '{print "\""$1"\""}'  | tr '\n' ',' | sed 's/.$//')
 
 mkdir http
 cd http
 cat > healthcheck.json<< EOF
 {
   "healthcheck":true,
+  "modules":$modules,
   "ip":"$eth0_ip",
   "hostname":"$eth0_hostname",
-  "modules":$modules,
-  "swap_total":$swap_total
+  "disks": { $disks },
+  "swap_total":$swap_total,
+  "env": { $envs }
 }
 EOF
 
 echo "vmify-image-healthcheck starting ..."
+cat healthcheck.json
 httpd -f -vv
