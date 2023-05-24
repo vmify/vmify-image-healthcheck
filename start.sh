a0d1c2e7fe40e004b6f7b1536fb9bdde7378a4d4
axel <684824+axelfontaine@users.noreply.github.com>
2023-05-24 16:28:41 +0200

    Added logging

--- start.sh
+++ start.sh
@@ -2,16 +2,21 @@
 
 echo "vmify-image-healthcheck collecting info ..."
 
+echo "-> kernel"
 if [ -f /proc/modules ]; then modules="true"; else modules="false"; fi
+readonly sysctls=$(sysctl -a | sed 's/ = /":"/' | awk '{print "\""$1"\""}' | tr '\n' ',' | sed 's/.$//')
 
-readonly user=$(whoami)
+echo "-> hardware"
 readonly bios_version=$(cat /sys/devices/virtual/dmi/id/bios_version)
 readonly chassis_asset_tag=$(cat /sys/devices/virtual/dmi/id/chassis_asset_tag)
+
+echo "-> network"
 readonly eth0_ip=$(ifconfig eth0 | grep 'inet ' | cut -d ':' -f 2 | cut -d ' ' -f 1)
 readonly eth0_hostname=$(hostname)
 readonly eth0_ntp_servers=$(cat /proc/net/ipconfig/ntp_servers)
 readonly etc_hosts=$(awk '{ printf "\"" $1 "\":\"";for (i=2; i<NF; i++) printf $i " "; printf $NF "\"," }' /etc/hosts | sed 's/.$//')
 
+echo "-> disks"
 readonly disks=$(tail +3 /proc/partitions | awk '{ print "\"" $4 "\":" $3*1024 }' | sort | tr '\n' ',' | sed 's/.$//')
 readonly mounts=$(awk '{ print "\"" $2 "\":\"" $1 " " $3 " " $4 " " $5 " " $6 "\"" }' /proc/mounts | sort | tr '\n' ',' | sed 's/.$//')
 
@@ -34,11 +39,14 @@ else
   readonly temp_kib=0
 fi
 
+echo "-> environment"
+readonly user=$(whoami)
 readonly envs=$(env | sort | sed 's/=/":"/' | awk '{print "\""$1"\""}' | tr '\n' ',' | sed 's/.$//')
-readonly sysctls=$(sysctl -a | sed 's/ = /":"/' | awk '{print "\""$1"\""}' | tr '\n' ',' | sed 's/.$//')
 readonly psax=$(ps ax)
 readonly pss=$(echo "$psax" | tail +2 | head -n -1 | awk '{$2=$3=""; print $0}' | tr -s " " | sed 's/ /":"/' | awk '{print "\"" $0 "\""}' | tr '\n' ',' | sed 's/.$//')
 readonly boot_timestamp=$(uptime -s)
+
+echo "vmify-image-healthcheck writing json ..."
 
 cat > healthcheck.json<< EOF
 {
@@ -63,7 +71,7 @@ cat > healthcheck.json<< EOF
   "ps":{$pss}
 }
 EOF
+cat healthcheck.json
 
 echo "vmify-image-healthcheck starting httpd ..."
-cat healthcheck.json
 timeout 10m httpd -f -vv -p 80
